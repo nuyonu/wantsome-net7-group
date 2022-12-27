e382dc40be111f3e5b032e297470856559d0979f
nuyonu <nuyonu@gmail.com>
2022-12-27 13:06:10 +0200

    Add homework solution

---
+++ OOP.Intro.Homework.Solution/OOP.Intro.Homework.Solution/ExerciseOne/Book.cs
@@ -0,0 +1,49 @@
+﻿namespace OOP.Intro.Homework.Solution.ExerciseOne
+{
+    public class Book
+    {
+        // Fields
+        private string title;
+        private string author;
+        private int year;
+
+        // Constructor
+        public Book(string title, string author, int year)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+        // Getters and setters for the fields
+        public string GetAuthor()
+        {
+            return this.author;
+        }
+
+        public string GetTitle()
+        {
+            return this.title;
+        }
+
+        public int GetYear()
+        {
+            return this.year;
+        }
+
+        public void SetAuthor(string newAuthor)
+        {
+            this.author = newAuthor;
+        }
+
+        public void SetTitle(string newTitle)
+        {
+            this.title = newTitle;
+        }
+
+        public void SetYear(int newYear)
+        {
+            this.year = newYear;
+        }
+    }
+}
---
+++ OOP.Intro.Homework.Solution/OOP.Intro.Homework.Solution/ExerciseTwo/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace OOP.Intro.Homework.Solution.ExerciseTwo
+{
+    public class Product
+    {
+        // Fields
+        private string name;
+        private double price;
+        private int quantity;
+
+        // Constructor
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        // Getters and setters for the fields
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+    }
+}
